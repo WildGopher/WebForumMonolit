b8e1ee11e98e7920db3e9db9502bcc0d63da37cc
WildGopher <90858483+WildGopher@users.noreply.github.com>
2021-09-19 11:17:49 +0300

    Add files via upload

--- App_Start/Startup.cs
+++ App_Start/Startup.cs
@@ -1,1 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+[assembly: OwinStartup(typeof(WebForum.App_Start.Startup))]
+namespace WebForum.App_Start
+{
+    public class Startup
+    {
 
+        
+        public void Configuration(IAppBuilder app)
+        {
+           
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+            
+        }
+        
+
+    }
+}
